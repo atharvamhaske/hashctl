2c7c483ba44745b5c95a7c257f09edaadbb0d44b
atharvamhaske <atharvamhaske76@gmail.com>
2026-02-21 19:43:18 +0530

    fix: show banner on 'hashctl' command, remove from --help flag

--- cmd/root.go
+++ cmd/root.go
@@ -2,6 +2,8 @@
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/atharvamhaske/hashctl/internal/tui"
 	"github.com/spf13/cobra"
 )
@@ -12,6 +14,8 @@ var rootCmd = &cobra.Command{
 	SilenceUsage:  true,
 	SilenceErrors: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
+		// Show banner before launching TUI
+		PrintBanner()
 		return tui.Run()
 	},
 }
@@ -26,8 +30,20 @@ func init() {
 	rootCmd.AddCommand(versionCmd)
 	rootCmd.AddCommand(checkCmd)
 
-	// Replace Cobra's default help with our PMG-style banner + usage
+	// Custom help without banner (banner only shows on `hashctl` command)
 	rootCmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
-		PrintUsage()
+		dim := tui.MutedStyle
+		val := tui.ValueStyle
+
+		fmt.Println()
+		fmt.Println(dim.Render("Usage:"))
+		fmt.Println(val.Render("  hashctl") + dim.Render("          launch interactive TUI"))
+		fmt.Println(val.Render("  hashctl list") + dim.Render("      list supported algorithms"))
+		fmt.Println(val.Render("  hashctl version") + dim.Render("   show version info"))
+		fmt.Println(val.Render("  hashctl check") + dim.Render("     check for updates"))
+		fmt.Println()
+		fmt.Println(dim.Render("Flags:"))
+		fmt.Println(dim.Render("  -h, --help   show this message"))
+		fmt.Println()
 	})
 }
